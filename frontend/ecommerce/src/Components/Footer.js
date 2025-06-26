cbac78ff8caad0551fd4f7e1e1d6b32f3bf09646
Cesar Roa <kezar1@gmail.com>
2025-06-25 20:57:26 -0400

    #5 feat: Footer component placeholders

--- frontend/ecommerce/src/Components/Footer.js
+++ frontend/ecommerce/src/Components/Footer.js
@@ -1,23 +1,71 @@
 import styled from 'styled-components';
+import FooterAccount from './FooterAccount';
+import FooterLinks from './FooterLinks';
+import FooterSupport from './FooterSupport';
+import DarkMode from './FooterDarkMode';
+import Country from './FooterCountry';
+import Language from './FooterLanguage';
 
 const Footer = () => {
     return(
         <Wrapper>
             <P>
-                Footer WIP
+                TúMandas
             </P>
+            <div>
+                <FooterAccount/>
+                <FooterSupport/>
+                <FooterLinks/>
+            </div>
+            <div className='buttons'>
+                <Language/>
+                <Country/>
+                <DarkMode/>
+            </div>
         </Wrapper>
     )
 }
 const P = styled.h1`
-  font-family: ${({ theme }) => theme.fonts.primary};
-  color: ${({ theme }) => theme.palette.primary};
+    font-family: ${({ theme }) => theme.fonts.primary};
+    color: ${({ theme }) => theme.palette.primary};
 `
 const Wrapper = styled.div`
-    background-color: darkblue;
+    font-size: 10px;
+    background-color: black;
     color: white;
-    border-style: dashed ;
-    border-color: red;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    &>.buttons{
+        width: 40vw;
+        justify-content: space-between;
+        display: flex;
+        flex-direction: row;
+        border: 2px solid grey;
+        border-radius: 30px;
+        font-size: 15px;
+        &>div{
+            height: 50px;
+            width: 50vw;
+            color: white;
+            justify-items: center;
+        };
+    }
+    &>div{
+        width: 80vw;
+        justify-content: space-between;
+        display: flex;
+        flex-direction: row;
+        border-top: 2px solid grey;
+        font-size: 12px;
+        &>div{
+            padding-top: 5px;
+            height: 150px;
+            width: 20vw;
+            color: white;
+            justify-items: center;
+        };
+    };
 `
 
 export default Footer
