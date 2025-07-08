5fecd6e99afef9958f4c7f31d9650d8b465a8f8e
Cesar Roa <kezar1@gmail.com>
2025-07-08 16:41:06 -0400

    fix(Kezare): add languages to footer & script object for whole site

---
+++ frontend/ecommerce/src/Objects/Script.js
@@ -0,0 +1,79 @@
+const pageScript = {
+    en: {
+        header: "Welcome to Tú Manda, your Mandala World",
+        subtitle: "Here, we color your life!",
+        peace: "Peace",
+        balance: "Balance",
+        harmony: "Harmony",
+        cta: "Begin Journey",
+        footer:{
+            account:{
+                h2:"My Account",
+                link2: "Sign In",
+                link3: "placeholder"
+            },
+            support:{
+                h2:"Custom Service",
+                link2:"placeholder",
+                link3:"placeholder"
+            },
+            social:{
+                h2:"Stay Connected",
+                link2:"placeholder",
+                link3:"placeholder"
+            }
+        }
+    },
+    fr: {
+        header: "Bienvenue à Tú Manda, ton Monde Mandala",
+        subtitle: "Ici, on colore ta vie!",
+        peace: "Paix",
+        balance: "Équilibre",
+        harmony: "Harmonie",
+        cta: "Commencer le Voyage",
+        footer:{
+            account:{
+                h2: "Mon Compte",
+                link2: "Se connecter",
+                link3: "placeholder"
+            },
+            support:{
+                h2:"Soutien technique",
+                link2:"placeholder",
+                link3:"placeholder"
+            },
+            social:{
+                h2:"Restez connecté",
+                link2:"placeholder",
+                link3:"placeholder"
+            }
+        }
+    },
+    es: {
+        header: "Bienvenido a Tú Manda, tu Mundo Mandala",
+        subtitle: "Aquí, coloremaons tu vida!",
+        peace: "Paz",
+        balance: "Equilibrio",
+        harmony: "Armonía",
+        cta: "Comenzar el Viaje",
+        footer: {
+            account:{
+            h2: "Mi Cuenta",
+            link2: "Iniciar Sesion",
+            link3: "placeholder"
+            },
+        support:{
+            h2:"Servicio Tecnico",
+            link2:"placeholder",
+            link3:"placeholder"
+            },
+        social:{
+            h2:"Nuestras Redes",
+            link2:"placeholder",
+            link3:"placeholder"
+            }
+        }
+    }
+};
+
+export default pageScript
